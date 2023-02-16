0908898edac5f1fa57fa182501a8f1b08a2332d3
ulvertrom <ulvertrom@gmail.com>
2023-02-16 12:42:30 +0400

    5 progs added

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int remainder = year % 4;
+        switch (remainder) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("not leap");
+                break;
+            case 4:
+                if ((year % 100) != 0) {
+                    System.out.println("leap");
+                }
+                else {
+                    System.out.println((year % 400 == 0) ? "leap": "not leap");
+                }
+        }
     }
 }
